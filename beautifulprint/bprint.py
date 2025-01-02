4fcc9837b8921b05e7faa2e881300ad1b4865238
rtk <ldashunderscoreo@gmail.com>
2025-01-02 14:31:31 +0000

    no need for them arg in _bprint

--- beautifulprint/bprint.py
+++ beautifulprint/bprint.py
@@ -11,11 +11,11 @@ def bprint(*args, theme: Theme = None, end: str = '\n', sep: str = '\n---\n'):
         for i, arg in enumerate(args):
             if i == len(args) - 1:
                 # For the final element, end with the end string instead of the seperator
-                _bprint(arg, theme, end)
+                _bprint(arg, end)
             else:
                 # Only add separators between elements
-                _bprint(arg, theme, sep)
+                _bprint(arg, sep)
 
 
-def _bprint(arg: object, theme=None, end: str = '\n'):
+def _bprint(arg: object, end: str = '\n'):
     print(bepr(arg), end=end)
